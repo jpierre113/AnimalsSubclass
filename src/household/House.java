5f2a14e1877da55fd4bab97111e842d9740a431c
Janna Pierre <jannapierre@gmail.com>
2019-09-20 08:34:53 -0400

    Created one inference and two classes.

---
+++ src/household/House.java
@@ -0,0 +1,10 @@
+package household;
+
+public interface House {
+//    int getIncomeAmount();
+//    int getincomeTax();
+    String getPetName();
+    void beFed();
+    void beGroomed();
+    void playWith();
+}
